c5c0f182c4ea69a92841b2e75b01b0e8f507dba4
not1cyyy <firas1120@gmail.com>
2022-05-25 18:04:53 +0100

    second time formatting the code

--- RSA-Generate.go
+++ RSA-Generate.go
@@ -1,85 +1,85 @@
 package main
 
 // this code is my very first code in golang when it comes to cryptography
-// I'm sorry for any inefficiency that is implemented here 
+// I'm sorry for any inefficiency that is implemented here
 // if you have any improvements don't hesitate to send a pull request
 
 import (
-    "crypto/rand"
-    "crypto/rsa"
-    "crypto/x509"
-    "encoding/pem"
-    "fmt"
-    "os"
+	"crypto/rand"
+	"crypto/rsa"
+	"crypto/x509"
+	"encoding/pem"
+	"fmt"
+	"os"
 )
 
 // karudo: w3
 
 func generate(length int) {
-    // here we generate the key
-    privatekey, err := rsa.GenerateKey(rand.Reader, length)
-    if err != nil {
-        fmt.Printf("Cannot generate RSA key, exiting...\n")
-        os.Exit(1)
-    }
-    publickey := &privatekey.PublicKey
+	// here we generate the key
+	privatekey, err := rsa.GenerateKey(rand.Reader, length)
+	if err != nil {
+		fmt.Printf("Cannot generate RSA key, exiting...\n")
+		os.Exit(1)
+	}
+	publickey := &privatekey.PublicKey
 
-    // now dump the private key to a file
-    var privateKeyBytes []byte = x509.MarshalPKCS1PrivateKey(privatekey)
-    PRIVblock := &pem.Block{
-        Type:  "RSA PRIVATE KEY",
-        Bytes: privateKeyBytes,
-    }
-    PEMPriv, err := os.Create("private.pem")
-    if err != nil {
-        fmt.Printf("error when creating the file private.pem: %s \n", err)
-        os.Exit(1)
-    }
-    err = pem.Encode(PEMPriv, PRIVblock)
-    if err != nil {
-        fmt.Printf("error when encoding the file private.pem: %s \n", err)
-        os.Exit(1)
-    }
-    // now we dump the public key to a file
-    publicKeyBytes, err := x509.MarshalPKIXPublicKey(publickey)
-    if err != nil {
-        fmt.Printf("error when dumping the public key: %s \n", err)
-        os.Exit(1)
-    }
-    PUBblock := &pem.Block{
-        Type:  "PUBLIC KEY",
-        Bytes: publicKeyBytes,
-    }
-    PEMPub, err := os.Create("public.pem")
-    if err != nil {
-        fmt.Printf("error when creating the file public.pem: %s \n", err)
-        os.Exit(1)
-    }
-    err = pem.Encode(PEMPub, PUBblock)
-    if err != nil {
-        fmt.Printf("error when encoding the file public.pem: %s \n", err)
-        os.Exit(1)
-    }
-fmt.Printf("Saved output to private.pem and public.pem \n")
+	// now dump the private key to a file
+	var privateKeyBytes []byte = x509.MarshalPKCS1PrivateKey(privatekey)
+	PRIVblock := &pem.Block{
+		Type:  "RSA PRIVATE KEY",
+		Bytes: privateKeyBytes,
+	}
+	PEMPriv, err := os.Create("private.pem")
+	if err != nil {
+		fmt.Printf("error when creating the file private.pem: %s \n", err)
+		os.Exit(1)
+	}
+	err = pem.Encode(PEMPriv, PRIVblock)
+	if err != nil {
+		fmt.Printf("error when encoding the file private.pem: %s \n", err)
+		os.Exit(1)
+	}
+	// now we dump the public key to a file
+	publicKeyBytes, err := x509.MarshalPKIXPublicKey(publickey)
+	if err != nil {
+		fmt.Printf("error when dumping the public key: %s \n", err)
+		os.Exit(1)
+	}
+	PUBblock := &pem.Block{
+		Type:  "PUBLIC KEY",
+		Bytes: publicKeyBytes,
+	}
+	PEMPub, err := os.Create("public.pem")
+	if err != nil {
+		fmt.Printf("error when creating the file public.pem: %s \n", err)
+		os.Exit(1)
+	}
+	err = pem.Encode(PEMPub, PUBblock)
+	if err != nil {
+		fmt.Printf("error when encoding the file public.pem: %s \n", err)
+		os.Exit(1)
+	}
+	fmt.Printf("Saved output to private.pem and public.pem \n")
 }
-func main(){
+func main() {
 
-var choice int
+	var choice int
 
-fmt.Println("Here are the available key lengths : ")
-fmt.Println("1 -> 512 bits")
-fmt.Println("2 -> 1024 bits")
-fmt.Println("3 -> 2048 bits")
-fmt.Print("Enter the number corresponding to the desired key length : ")
+	fmt.Println("Here are the available key lengths : ")
+	fmt.Println("1 -> 512 bits")
+	fmt.Println("2 -> 1024 bits")
+	fmt.Println("3 -> 2048 bits")
+	fmt.Print("Enter the number corresponding to the desired key length : ")
 
-fmt.Scanln(&choice)
+	fmt.Scanln(&choice)
 
-switch choice {
-case 1:
-    generate(512);
-case 2:
-    generate(1024);
-default:
-    generate(2048);
-}
+	switch choice {
+	case 1:
+		generate(512)
+	case 2:
+		generate(1024)
+	default:
+		generate(2048)
+	}
 }
