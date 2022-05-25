7437c27cb92c2e842d43f9a7320dfa97e3f220c7
not1cyyy <firas1120@gmail.com>
2022-05-25 17:07:24 +0100

    added a switch statement to choose between different key lengths

--- RSA-Generate.go
+++ RSA-Generate.go
@@ -13,7 +13,103 @@ import (
     "os"
 )
 
-func main() {
+func case1() {
+    // here we generate the key
+    privatekey, err := rsa.GenerateKey(rand.Reader, 512)
+    if err != nil {
+        fmt.Printf("Cannot generate RSA key, exiting...\n")
+        os.Exit(1)
+    }
+    publickey := &privatekey.PublicKey
+
+    // now dump the private key to a file
+    var privateKeyBytes []byte = x509.MarshalPKCS1PrivateKey(privatekey)
+    PRIVblock := &pem.Block{
+        Type:  "RSA PRIVATE KEY",
+        Bytes: privateKeyBytes,
+    }
+    PEMPriv, err := os.Create("private.pem")
+    if err != nil {
+        fmt.Printf("error when creating the file private.pem: %s \n", err)
+        os.Exit(1)
+    }
+    err = pem.Encode(PEMPriv, PRIVblock)
+    if err != nil {
+        fmt.Printf("error when encoding the file private.pem: %s \n", err)
+        os.Exit(1)
+    }
+    // now we dump the public key to a file
+    publicKeyBytes, err := x509.MarshalPKIXPublicKey(publickey)
+    if err != nil {
+        fmt.Printf("error when dumping the public key: %s \n", err)
+        os.Exit(1)
+    }
+    PUBblock := &pem.Block{
+        Type:  "PUBLIC KEY",
+        Bytes: publicKeyBytes,
+    }
+    PEMPub, err := os.Create("public.pem")
+    if err != nil {
+        fmt.Printf("error when creating the file public.pem: %s \n", err)
+        os.Exit(1)
+    }
+    err = pem.Encode(PEMPub, PUBblock)
+    if err != nil {
+        fmt.Printf("error when encoding the file public.pem: %s \n", err)
+        os.Exit(1)
+    }
+fmt.Printf("Saved output to private.pem and public.pem \n")
+}
+
+func case2() {
+    // here we generate the key
+    privatekey, err := rsa.GenerateKey(rand.Reader, 1024)
+    if err != nil {
+        fmt.Printf("Cannot generate RSA key, exiting...\n")
+        os.Exit(1)
+    }
+    publickey := &privatekey.PublicKey
+
+    // now dump the private key to a file
+    var privateKeyBytes []byte = x509.MarshalPKCS1PrivateKey(privatekey)
+    PRIVblock := &pem.Block{
+        Type:  "RSA PRIVATE KEY",
+        Bytes: privateKeyBytes,
+    }
+    PEMPriv, err := os.Create("private.pem")
+    if err != nil {
+        fmt.Printf("error when creating the file private.pem: %s \n", err)
+        os.Exit(1)
+    }
+    err = pem.Encode(PEMPriv, PRIVblock)
+    if err != nil {
+        fmt.Printf("error when encoding the file private.pem: %s \n", err)
+        os.Exit(1)
+    }
+    // now we dump the public key to a file
+    publicKeyBytes, err := x509.MarshalPKIXPublicKey(publickey)
+    if err != nil {
+        fmt.Printf("error when dumping the public key: %s \n", err)
+        os.Exit(1)
+    }
+    PUBblock := &pem.Block{
+        Type:  "PUBLIC KEY",
+        Bytes: publicKeyBytes,
+    }
+    PEMPub, err := os.Create("public.pem")
+    if err != nil {
+        fmt.Printf("error when creating the file public.pem: %s \n", err)
+        os.Exit(1)
+    }
+    err = pem.Encode(PEMPub, PUBblock)
+    if err != nil {
+        fmt.Printf("error when encoding the file public.pem: %s \n", err)
+        os.Exit(1)
+    }
+fmt.Printf("Saved output to private.pem and public.pem \n")
+}
+
+func defaul() {
     // here we generate the key
     privatekey, err := rsa.GenerateKey(rand.Reader, 2048)
     if err != nil {
@@ -59,4 +155,21 @@ func main() {
         os.Exit(1)
     }
 fmt.Printf("Saved output to private.pem and public.pem \n")
+}
+func main(){
+var choice int
+fmt.Println("Here are the available key lengths : ")
+fmt.Println("1 -> 512 bits")
+fmt.Println("2 -> 1024 bits")
+fmt.Println("3 -> 2048 bits")
+fmt.Print("Enter the number corresponding to the desired key length : ")
+fmt.Scanln(&choice)
+switch choice {
+case 1:
+    case1()
+case 2:
+    case2()
+default:
+    defaul()
+}
 }
